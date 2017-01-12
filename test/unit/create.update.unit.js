77a20011949e505a993f491e720a9e2077ec2f13
Viktor Chobanov <chobanov10100@mail.bg>
2017-01-12 11:10:31 +0200

    CRUD unit tests

--- test/unit/create.update.unit.js
+++ test/unit/create.update.unit.js
@@ -9,24 +9,38 @@ var should = require('should'),
 
 describe('Connector CREATE and UPDATE', () => {
     var self = this;
+    var _id;
 
     it("should be able to create objects", (next) => {
         const _model = Arrow.Model.getModel('appc.loki.js/users');
         const newModel = {
             name: 'Fiodor Zaiuchuk',
-            weapons: ["ram"],
-            Age: 44
+            weapons: "['ram']",
+            Age: "44"
         };
         let __model = Arrow.Model.getModel('appc.loki.js/users');
         //create a new model
         __model.create(newModel, (_error, _instance) => {
-            dump(_instance);
+            var __instance = _instance.toPayload();
+
             should(_error).be.not.ok;
-            should(_instance).be.ok;
-            should(_instance.getPrimaryKey()).be.a.String;
-            should(_instance.getPrimaryKey().length).be.greaterThan(0);
+            should(__instance).be.ok;
+            _id = __instance.$loki;
+            should(__instance.$loki).be.a.String;
             next();
         });
+    });
+
+    after("", (next) => {
+        const _model = Arrow.Model.getModel('appc.loki.js/users');
+        _model.delete(_id, (err, resp) => {
+            should(err).not.be.ok;
+            _model.findByID(_id, (err, resp) => {
+                should(err).be.ok;
+                next();
+            });
+        });
+
     });
 
 });
